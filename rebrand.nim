893a6cec777212d9d067fc7384c21813cb543b56
Gruruya <gruruya.chi4c@slmails.com>
2023-07-09 15:01:24 -0400

    Add short descriptions of files to headers

--- rebrand.nim
+++ rebrand.nim
@@ -1,3 +1,4 @@
+# Rebranding script --- nimtemplate
 # Copyright © 2023 Gruruya <gruruya.chi4c@slmails.com>
 # SPDX-License-Identifier: AGPL-3.0-only
 
@@ -10,15 +11,12 @@ if input.len notin {1, 3}:
   echo "Usage: rebrand PROJECT_NAME [<AUTHOR> <EMAIL>]"
   quit 1
 
-let project = input[0]
-for file in ["rebrand.nim", "README.md", "nimtemplate.nimble", "tests/test.nim", ".github/workflows/documentation.yml"]:
-  file.writeFile file.readFile.replace("nimtemplate", project)
-
 if input.len == 3:
-  let author = input[1]
-  let email = input[2]
-  for file in ["README.md", "nimtemplate.nimble", "rebrand.nim", "src/nimtemplate.nim", "src/nimtemplate/common.nim", "tests/test.nim", ".gitignore", ".github/dependabot.yml", ".github/workflows/build.yml", ".github/workflows/documentation.yml", ".github/workflows/nim-run/action.yml"]:
-    file.writeFile file.readFile.multiReplace(("gruruya.chi4c@slmails.com", email), ("Gruruya", author))
+  for file in ["rebrand.nim", "README.md", "nimtemplate.nimble", "src/nimtemplate.nim", "src/nimtemplate/common.nim", "tests/test.nim", ".github/dependabot.yml", ".github/workflows/build.yml", ".github/workflows/documentation.yml", ".github/workflows/nim-run/action.yml", ".gitignore"]:
+    file.writeFile file.readFile.multiReplace(("nimtemplate", input[0]), ("gruruya.chi4c@slmails.com", input[2]), ("Gruruya", input[1]))
+else:
+  for file in ["rebrand.nim", "README.md", "nimtemplate.nimble", "src/nimtemplate.nim", "src/nimtemplate/common.nim", "tests/test.nim", ".github/dependabot.yml", ".github/workflows/build.yml", ".github/workflows/documentation.yml", ".github/workflows/nim-run/action.yml"]:
+    file.writeFile file.readFile.replace("nimtemplate", input[0])
 
 moveFile("nimtemplate.nimble", project & ".nimble")
 moveFile("src/nimtemplate.nim", "src/" & project & ".nim")
