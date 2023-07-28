89e47cfec4d0939df81672a763c43c25edb26f54
Gruruya <gruruya.chi4c@slmails.com>
2023-07-28 19:09:10 -0400

    Delete extra src folder

--- src/nimtemplate.nim
+++
@@ -1,5 +0,0 @@
-# Main file --- nimtemplate
-# Copyright © 2023 Gruruya <gruruya.chi4c@slmails.com>
-# SPDX-License-Identifier: AGPL-3.0-only
-
-## This file should probably be a public interface to your library.
--- src/nimtemplate/common.nim
+++
@@ -1,5 +0,0 @@
-# Common utilities --- nimtemplate
-# Copyright © 2023 Gruruya <gruruya.chi4c@slmails.com>
-# SPDX-License-Identifier: AGPL-3.0-only
-
-## Add private variables or functions here that you don't want to export.
