92412327e1cae44d64fc375499cb9d088dc05df2
Gruruya <gruruya.chi4c@slmails.com>
2023-07-28 18:40:24 -0400

    track files

---
+++ nimtemplate.nim
@@ -0,0 +1,7 @@
+# nimtemplate.nim - Main file for Nimtemplate, a Nim template repository.
+# Copyright © 2023 Gruruya <gruruya.chi4c@slmails.com>
+# SPDX-License-Identifier: AGPL-3.0-only
+
+## This file should probably be a public interface to your library.
+
+# nimtemplate.nim ends here
---
+++ nimtemplate/common.nim
@@ -0,0 +1,7 @@
+# common.nim - Private utilities for Nimtemplate, a Nim template repository.
+# Copyright © 2023 Gruruya <gruruya.chi4c@slmails.com>
+# SPDX-License-Identifier: AGPL-3.0-only
+
+## Add private variables or functions here that you don't want to export.
+
+# common.nim ends here
